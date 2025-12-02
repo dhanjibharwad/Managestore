0da162c437e4ac17d2ac30731ca97f3e6abfaaae
ainops <shareroommunich@gmail.com>
2025-12-02 10:19:00 +0530

    changes

--- app/home/services/page.tsx
+++ app/home/services/page.tsx
@@ -73,12 +73,12 @@ const services: ServiceCardProps[] = [
 
 const ServicesPage = () => {
   return (
-    <section className="py-10 bg-zinc-800">
+    <section className="py-10 bg-white">
       <div className="text-center mb-12">
-        {/* <h2 className="text-3xl font-bold text-gray-800">Our Services</h2> */}
-        {/* <p className="mt-4 text-gray-500 max-w-2xl mx-auto">
+        <h2 className="text-3xl font-bold text-gray-800">Our Services</h2>
+        <p className="mt-4 text-gray-500 max-w-2xl mx-auto">
           Comprehensive support solutions designed to meet all your technical and service needs with professional expertise.
-        </p> */}
+        </p>
       </div>
 
       <div className="container mx-auto px-6 lg:px-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
