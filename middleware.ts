4511882ef92c47f95b677a34a6d19e0f237a4b71
dhanjibharwad <dhanjibharwad94@gmail.com>
2026-01-20 17:38:02 +0530

    check again

--- middleware.ts
+++ middleware.ts
@@ -81,7 +81,10 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/admin/:path*',
+    '/technician/:path*',
+    '/customer/:path*',
+    '/super-admin/:path*'
   ],
 };
 
