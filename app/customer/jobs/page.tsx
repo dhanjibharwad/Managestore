74ee2aa61e4c4d78ccf03679d91154793e132850
dhanjibharwad <dhanjibharwad94@gmail.com>
2026-01-21 10:27:09 +0530

    changes

--- app/customer/jobs/page.tsx
+++ app/customer/jobs/page.tsx
@@ -44,7 +44,7 @@ export default function JobsPage() {
                 Open Jobs
               </Link>
               <Link
-                href="/customer/jobs/all"
+                href="/customer/jobs/all/"
                 className={`py-4 text-sm font-medium border-b-2 transition-colors ${
                   activeTab === 'all'
                     ? 'border-[#4A70A9] text-[#4A70A9]'
