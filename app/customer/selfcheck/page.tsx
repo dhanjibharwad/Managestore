833cab33ee827f548932e898ae000306e75e485e
dhanjibharwad <dhanjibharwad94@gmail.com>
2026-02-16 18:05:30 +0530

    check system in self check in

--- app/customer/selfcheck/page.tsx
+++ app/customer/selfcheck/page.tsx
@@ -451,7 +451,7 @@ export default function SelfCheckIn() {
         ))}
       </div>
       {/* Header */}
-      <div className="bg-white border-b border-gray-200">
+      <div className="bg-white border-b border-gray-200 rounded-xl">
         <div className="max-w-7xl mx-auto px-4 py-4">
           <div className="flex items-center justify-between mb-6">
             <div className="flex items-center gap-2">
